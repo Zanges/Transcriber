69ccb651d5eb8783e20c9a83cbd9a99083b05bb7
Zanges <3868054+Zanges@users.noreply.github.com>
2024-08-31 21:44:17 +0200

    start the event handler

--- rust_transcriber/src/main.rs
+++ rust_transcriber/src/main.rs
@@ -5,6 +5,7 @@ mod openai_transcribe;
 mod output_handler;
 mod main_gui;
 
+use winit::event_loop::EventLoop;
 use std::sync::{Arc, Mutex};
 use config_handler::Config;
 use output_handler::OutputHandler;
@@ -20,6 +21,9 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     let openai_transcriber = Arc::new(OpenAITranscriber::new(config.openai_api_key.clone()));
     let output_handler = Arc::new(OutputHandler::new(config.word_delay, config.key_event_delay));
     let hotkey_handler = HotkeyHandler::new(&config.hotkey, Some(audio_recorder.clone()), Some(openai_transcriber.clone()), Some(output_handler.clone()))?;
+
+    let event_loop = EventLoop::new();
+    hotkey_handler.handle_events(event_loop);
 
     // Run the GUI
     Ok(run_gui(config)?)
