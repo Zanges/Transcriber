71d418930870ba875cd7599c295ee2d8de469995
Zanges <3868054+Zanges@users.noreply.github.com>
2024-08-31 03:13:06 +0200

    feat: Remove send_enter function from OutputHandler

--- rust_transcriber/src/output_handler.rs
+++ rust_transcriber/src/output_handler.rs
@@ -14,7 +14,6 @@ impl OutputHandler {
             self.send_char(c);
             thread::sleep(time::Duration::from_millis(10)); // Add a small delay between keystrokes
         }
-        self.send_enter(); // Send an Enter key press at the end
     }
 
     fn send_char(&self, c: char) {
@@ -43,42 +42,6 @@ impl OutputHandler {
                 wVk: 0,
                 wScan: c as u16,
                 dwFlags: 0x0004 | 0x0002, // KEYEVENTF_UNICODE | KEYEVENTF_KEYUP
-                time: 0,
-                dwExtraInfo: 0,
-            };
-        }
-
-        unsafe {
-            SendInput(1, &mut input, std::mem::size_of::<INPUT>() as i32);
-        }
-    }
-
-    fn send_enter(&self) {
-        let mut input = INPUT {
-            type_: INPUT_KEYBOARD,
-            u: unsafe { std::mem::zeroed() },
-        };
-        
-        unsafe {
-            *input.u.ki_mut() = KEYBDINPUT {
-                wVk: VK_RETURN as u16,
-                wScan: 0,
-                dwFlags: 0,
-                time: 0,
-                dwExtraInfo: 0,
-            };
-        }
-
-        unsafe {
-            SendInput(1, &mut input, std::mem::size_of::<INPUT>() as i32);
-        }
-
-        // Key up event
-        unsafe {
-            *input.u.ki_mut() = KEYBDINPUT {
-                wVk: VK_RETURN as u16,
-                wScan: 0,
-                dwFlags: 0x0002, // KEYEVENTF_KEYUP
                 time: 0,
                 dwExtraInfo: 0,
             };
